c6efbf36d8458af57d7e50df06a0a10d7787db61
ghoulboy <gkatsaros16@gmail.com>
2019-08-06 20:37:17 -0400

    initial set up

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormsModule, ReactiveFormsModule, FormGroup, FormBuilder, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'twitchcon';
+export class AppComponent implements OnInit {
+  
+  entryForm: FormGroup;
+
+  constructor(
+    private formBuilder: FormBuilder,
+  ) {}
+
+  ngOnInit() {
+    this.entryForm = new FormGroup({
+      email: new FormControl(''),
+      twitchName: new FormControl(''),
+      youtubeName: new FormControl(''),
+      code: new FormControl('')
+    });
+  }
+
+  onSubmit(data) {
+    console.log(data);
+  }
 }
